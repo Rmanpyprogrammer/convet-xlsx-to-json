53dfa6d295690d810b4a62ab4c962d19715e6ccc
arman <arfly78@gmail.com>
2024-04-07 23:19:56 +0330

    update xlsx 2 jsn

--- excell_to _json.py
+++ excell_to _json.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-with open('backup_1.json', 'r') as f:
-    data = f.read()
+with pd.ExcelFile("1.xlsx") as x:
+    df=pd.read_excel(x)
+    
+df.to_json('output.json', orient='records')
 
-df = pd.read_json(data)
-
-
-with pd.ExcelWriter("1.xlsx",mode="a",if_sheet_exists="replace") as writer:
-    df.to_excel(writer,sheet_name="1",index=False)
